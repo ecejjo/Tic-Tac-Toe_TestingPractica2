4dfed4e0573f156c79e1c924ea781cb2d9676b9e
Francisco Javier Jiménez Olivares <francisco.javier.jimenez@ericsson.com>
2019-04-25 20:56:38 +0200

    Player1WinsTest, Player1LoosesTest, DrawTest coded.

--- src/test/java/es/codeurjc/ais/tictactoe/TicTacToeEndToEndWithSeleniumTests.java
+++ src/test/java/es/codeurjc/ais/tictactoe/TicTacToeEndToEndWithSeleniumTests.java
@@ -1,6 +1,9 @@
 package es.codeurjc.ais.tictactoe;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -24,12 +27,18 @@ public class TicTacToeEndToEndWithSeleniumTests {
 	Para obtener el mensaje del alert se utiliza el código browser1.switchTo().alert().getText()
 	 */
 	
-	protected WebDriver driverPlayer1, driverPlayer2;
+	private WebDriver driverPlayer1, driverPlayer2;
 	
 	@BeforeClass
 	public static void setupClass() {
 		WebDriverManager.chromedriver().setup();
+		WebApp.start();
 	 }
+	
+	@AfterClass
+	public static void teardownClass() {
+		WebApp.stop();
+	}
 	
 	 @Before
 	 public void setupTest() {
@@ -49,31 +58,134 @@ public class TicTacToeEndToEndWithSeleniumTests {
 	 }
 	 
 	 @Test
-	 public void test () throws InterruptedException {
+	 public void Player1WinsTest () throws InterruptedException {
 
 		 driverPlayer1.get("http://localhost:8080/");
-		 Thread.sleep(3000);  // Let the user actually see something!
-
-		 WebElement nicknamePlayer1 = driverPlayer1.findElement(By.id("nickname"));
-		 nicknamePlayer1.sendKeys("Player1");
-		 Thread.sleep(3000);  // Let the user actually see something!
-
-		 WebElement playButtonPlayer1 = driverPlayer1.findElement(By.id("startBtn"));
-		 playButtonPlayer1.click();
-		 Thread.sleep(3000);  // Let the user actually see something!
+		 WebElement nickname = driverPlayer1.findElement(By.id("nickname"));
+		 nickname.sendKeys("Player1");
+		 WebElement playButton = driverPlayer1.findElement(By.id("startBtn"));
+		 playButton.click();
 
 		 driverPlayer2.get("http://localhost:8080/");
-		 Thread.sleep(3000);  // Let the user actually see something!
+		 nickname = driverPlayer2.findElement(By.id("nickname"));
+		 nickname.sendKeys("Player2");
+		 playButton = driverPlayer2.findElement(By.id("startBtn"));
+		 playButton.click();
+		 
+		 WebElement cell = driverPlayer1.findElement(By.id("cell-0"));
+		 cell.click();
+		 
+		 cell = driverPlayer2.findElement(By.id("cell-1"));
+		 cell.click();
+		 
+		 cell = driverPlayer1.findElement(By.id("cell-3"));
+		 cell.click();
+		 
+		 cell = driverPlayer2.findElement(By.id("cell-4"));
+		 cell.click();
 
-		 WebElement nicknamePlayer2 = driverPlayer2.findElement(By.id("nickname"));
-		 nicknamePlayer2.sendKeys("Player2");
-		 Thread.sleep(3000);  // Let the user actually see something!
+		 cell = driverPlayer1.findElement(By.id("cell-6"));
+		 cell.click();
+		 
+		 Thread.sleep(1000);  // Gives time to show the alert
 
-		 WebElement playButtonPlayer2 = driverPlayer2.findElement(By.id("startBtn"));
-		 playButtonPlayer2.click();
-		 Thread.sleep(3000);  // Let the user actually see something!
-
-		 Thread.sleep(5000);  // Let the user actually see something!
+		 String messagePlayer1 = driverPlayer1.switchTo().alert().getText();
+		 String messagePlayer2 = driverPlayer2.switchTo().alert().getText();
+		 
+		 assertThat(messagePlayer1).isEqualTo("Player1 wins! Player2 looses.");
+		 assertThat(messagePlayer2).isEqualTo("Player1 wins! Player2 looses.");
 	 }
+	 
+	 @Test
+	 public void Player1LoosesTest () throws InterruptedException {
 
+		 driverPlayer1.get("http://localhost:8080/");
+		 WebElement nickname = driverPlayer1.findElement(By.id("nickname"));
+		 nickname.sendKeys("Player1");
+		 WebElement playButton = driverPlayer1.findElement(By.id("startBtn"));
+		 playButton.click();
+
+		 driverPlayer2.get("http://localhost:8080/");
+		 nickname = driverPlayer2.findElement(By.id("nickname"));
+		 nickname.sendKeys("Player2");
+		 playButton = driverPlayer2.findElement(By.id("startBtn"));
+		 playButton.click();
+		 
+		 WebElement cell = driverPlayer1.findElement(By.id("cell-0"));
+		 cell.click();
+		 
+		 cell = driverPlayer2.findElement(By.id("cell-1"));
+		 cell.click();
+		 
+		 cell = driverPlayer1.findElement(By.id("cell-3"));
+		 cell.click();
+		 
+		 cell = driverPlayer2.findElement(By.id("cell-4"));
+		 cell.click();
+
+		 cell = driverPlayer1.findElement(By.id("cell-8"));
+		 cell.click();
+		 
+		 cell = driverPlayer2.findElement(By.id("cell-7"));
+		 cell.click();
+		 
+		 Thread.sleep(1000);  // Gives time to show the alert
+
+		 String messagePlayer1 = driverPlayer1.switchTo().alert().getText();
+		 String messagePlayer2 = driverPlayer2.switchTo().alert().getText();
+		 
+		 assertThat(messagePlayer1).isEqualTo("Player2 wins! Player1 looses.");
+		 assertThat(messagePlayer2).isEqualTo("Player2 wins! Player1 looses.");
+	 }
+	 
+	 @Test
+	 public void DrawTest () throws InterruptedException {
+
+		 driverPlayer1.get("http://localhost:8080/");
+		 WebElement nickname = driverPlayer1.findElement(By.id("nickname"));
+		 nickname.sendKeys("Player1");
+		 WebElement playButton = driverPlayer1.findElement(By.id("startBtn"));
+		 playButton.click();
+
+		 driverPlayer2.get("http://localhost:8080/");
+		 nickname = driverPlayer2.findElement(By.id("nickname"));
+		 nickname.sendKeys("Player2");
+		 playButton = driverPlayer2.findElement(By.id("startBtn"));
+		 playButton.click();
+		 
+		 WebElement cell = driverPlayer1.findElement(By.id("cell-0"));
+		 cell.click();
+		 
+		 cell = driverPlayer2.findElement(By.id("cell-1"));
+		 cell.click();
+		 
+		 cell = driverPlayer1.findElement(By.id("cell-3"));
+		 cell.click();
+		 
+		 cell = driverPlayer2.findElement(By.id("cell-4"));
+		 cell.click();
+
+		 cell = driverPlayer1.findElement(By.id("cell-2"));
+		 cell.click();
+		 
+		 cell = driverPlayer2.findElement(By.id("cell-8"));
+		 cell.click();
+		 
+		 cell = driverPlayer1.findElement(By.id("cell-7"));
+		 cell.click();
+		 
+		 cell = driverPlayer2.findElement(By.id("cell-6"));
+		 cell.click();
+		 
+		 cell = driverPlayer1.findElement(By.id("cell-5"));
+		 cell.click();
+		 
+		 Thread.sleep(1000);  // Gives time to show the alert
+
+		 String messagePlayer1 = driverPlayer1.switchTo().alert().getText();
+		 String messagePlayer2 = driverPlayer2.switchTo().alert().getText();
+		 
+		 assertThat(messagePlayer1).isEqualTo("Draw!");
+		 assertThat(messagePlayer2).isEqualTo("Draw!");
+	 }
 }
