9140ea1cc42b5ac8c4324d600497f9a324ec67d1
Francisco Javier Jiménez Olivares <francisco.javier.jimenez@ericsson.com>
2019-04-26 00:40:43 +0200

    3x system test cases for each situation with Selenium

--- src/test/java/es/codeurjc/ais/tictactoe/TicTacToeSystemTest.java
+++ src/test/java/es/codeurjc/ais/tictactoe/TicTacToeSystemTest.java
@@ -57,9 +57,7 @@ public class TicTacToeSystemTest {
 		 }
 	 }
 	 
-	 @Test
-	 public void Player1WinsTest () throws InterruptedException {
-
+	 public void startGame() {
 		 driverPlayer1.get("http://localhost:8080/");
 		 WebElement nickname = driverPlayer1.findElement(By.id("nickname"));
 		 nickname.sendKeys("Player1");
@@ -70,122 +68,171 @@ public class TicTacToeSystemTest {
 		 nickname = driverPlayer2.findElement(By.id("nickname"));
 		 nickname.sendKeys("Player2");
 		 playButton = driverPlayer2.findElement(By.id("startBtn"));
-		 playButton.click();
-		 
-		 WebElement cell = driverPlayer1.findElement(By.id("cell-0"));
+		 playButton.click();		 
+	 }
+	 
+	 public void clickOnCell(WebDriver webDriver, int cellNumber) {
+		 WebElement cell = webDriver.findElement(By.id("cell-" + cellNumber));
 		 cell.click();
-		 
-		 cell = driverPlayer2.findElement(By.id("cell-1"));
-		 cell.click();
-		 
-		 cell = driverPlayer1.findElement(By.id("cell-3"));
-		 cell.click();
-		 
-		 cell = driverPlayer2.findElement(By.id("cell-4"));
-		 cell.click();
-
-		 cell = driverPlayer1.findElement(By.id("cell-6"));
-		 cell.click();
-		 
-		 Thread.sleep(1000);  // Gives time to show the alert
-
+	 }
+	 
+	 public void verifyMessages(String expectedMessage) {
 		 String messagePlayer1 = driverPlayer1.switchTo().alert().getText();
 		 String messagePlayer2 = driverPlayer2.switchTo().alert().getText();
 		 
-		 assertThat(messagePlayer1).isEqualTo("Player1 wins! Player2 looses.");
-		 assertThat(messagePlayer2).isEqualTo("Player1 wins! Player2 looses.");
+		 assertThat(messagePlayer1).isEqualTo(expectedMessage);
+		 assertThat(messagePlayer2).isEqualTo(expectedMessage);		 
 	 }
 	 
 	 @Test
-	 public void Player1LoosesTest () throws InterruptedException {
-
-		 driverPlayer1.get("http://localhost:8080/");
-		 WebElement nickname = driverPlayer1.findElement(By.id("nickname"));
-		 nickname.sendKeys("Player1");
-		 WebElement playButton = driverPlayer1.findElement(By.id("startBtn"));
-		 playButton.click();
-
-		 driverPlayer2.get("http://localhost:8080/");
-		 nickname = driverPlayer2.findElement(By.id("nickname"));
-		 nickname.sendKeys("Player2");
-		 playButton = driverPlayer2.findElement(By.id("startBtn"));
-		 playButton.click();
+	 public void Player1WinsTest1 () throws InterruptedException {
 		 
-		 WebElement cell = driverPlayer1.findElement(By.id("cell-0"));
-		 cell.click();
-		 
-		 cell = driverPlayer2.findElement(By.id("cell-1"));
-		 cell.click();
-		 
-		 cell = driverPlayer1.findElement(By.id("cell-3"));
-		 cell.click();
-		 
-		 cell = driverPlayer2.findElement(By.id("cell-4"));
-		 cell.click();
-
-		 cell = driverPlayer1.findElement(By.id("cell-8"));
-		 cell.click();
-		 
-		 cell = driverPlayer2.findElement(By.id("cell-7"));
-		 cell.click();
-		 
+		 startGame();
+		 clickOnCell(driverPlayer1, 0);
+		 clickOnCell(driverPlayer2, 1);
+		 clickOnCell(driverPlayer1, 3);
+		 clickOnCell(driverPlayer2, 4);
+		 clickOnCell(driverPlayer1, 6);
+		 		 
 		 Thread.sleep(1000);  // Gives time to show the alert
-
-		 String messagePlayer1 = driverPlayer1.switchTo().alert().getText();
-		 String messagePlayer2 = driverPlayer2.switchTo().alert().getText();
 		 
-		 assertThat(messagePlayer1).isEqualTo("Player2 wins! Player1 looses.");
-		 assertThat(messagePlayer2).isEqualTo("Player2 wins! Player1 looses.");
+		 verifyMessages("Player1 wins! Player2 looses.");
 	 }
 	 
 	 @Test
-	 public void DrawTest () throws InterruptedException {
+	 public void Player1WinsTest2 () throws InterruptedException {
+		 
+		 startGame();
+		 clickOnCell(driverPlayer1, 6);
+		 clickOnCell(driverPlayer2, 0);
+		 clickOnCell(driverPlayer1, 7);
+		 clickOnCell(driverPlayer2, 4);
+		 clickOnCell(driverPlayer1, 8);
+		 
+		 Thread.sleep(1000);  // Gives time to show the alert
+		 
+		 verifyMessages("Player1 wins! Player2 looses.");
+	 }
 
-		 driverPlayer1.get("http://localhost:8080/");
-		 WebElement nickname = driverPlayer1.findElement(By.id("nickname"));
-		 nickname.sendKeys("Player1");
-		 WebElement playButton = driverPlayer1.findElement(By.id("startBtn"));
-		 playButton.click();
+	 @Test
+	 public void Player1WinsTest3 () throws InterruptedException {
+		 
+		 startGame();
+		 clickOnCell(driverPlayer1, 1);
+		 clickOnCell(driverPlayer2, 2);
+		 clickOnCell(driverPlayer1, 4);
+		 clickOnCell(driverPlayer2, 3);
+		 clickOnCell(driverPlayer1, 7);
+		 
+		 Thread.sleep(1000);  // Gives time to show the alert
+		 
+		 verifyMessages("Player1 wins! Player2 looses.");
+	 }
 
-		 driverPlayer2.get("http://localhost:8080/");
-		 nickname = driverPlayer2.findElement(By.id("nickname"));
-		 nickname.sendKeys("Player2");
-		 playButton = driverPlayer2.findElement(By.id("startBtn"));
-		 playButton.click();
-		 
-		 WebElement cell = driverPlayer1.findElement(By.id("cell-0"));
-		 cell.click();
-		 
-		 cell = driverPlayer2.findElement(By.id("cell-1"));
-		 cell.click();
-		 
-		 cell = driverPlayer1.findElement(By.id("cell-3"));
-		 cell.click();
-		 
-		 cell = driverPlayer2.findElement(By.id("cell-4"));
-		 cell.click();
+	 
+	 @Test
+	 public void Player1LoosesTest1 () throws InterruptedException {
 
-		 cell = driverPlayer1.findElement(By.id("cell-2"));
-		 cell.click();
+		 startGame();
+		 clickOnCell(driverPlayer1, 0);
+		 clickOnCell(driverPlayer2, 1);
+		 clickOnCell(driverPlayer1, 3);
+		 clickOnCell(driverPlayer2, 4);
+		 clickOnCell(driverPlayer1, 8);
+		 clickOnCell(driverPlayer2, 7);
 		 
-		 cell = driverPlayer2.findElement(By.id("cell-8"));
-		 cell.click();
+		 Thread.sleep(1000);  // Gives time to show the alert
 		 
-		 cell = driverPlayer1.findElement(By.id("cell-7"));
-		 cell.click();
+		 verifyMessages("Player2 wins! Player1 looses.");
+	 }
+
+	 @Test
+	 public void Player1LoosesTest2 () throws InterruptedException {
+
+		 startGame();
+		 clickOnCell(driverPlayer1, 0);
+		 clickOnCell(driverPlayer2, 3);
+		 clickOnCell(driverPlayer1, 7);
+		 clickOnCell(driverPlayer2, 4);
+		 clickOnCell(driverPlayer1, 2);
+		 clickOnCell(driverPlayer2, 5);
 		 
-		 cell = driverPlayer2.findElement(By.id("cell-6"));
-		 cell.click();
+		 Thread.sleep(1000);  // Gives time to show the alert
 		 
-		 cell = driverPlayer1.findElement(By.id("cell-5"));
-		 cell.click();
+		 verifyMessages("Player2 wins! Player1 looses.");
+	 }
+
+	 @Test
+	 public void Player1LoosesTest3 () throws InterruptedException {
+
+		 startGame();
+		 clickOnCell(driverPlayer1, 1);
+		 clickOnCell(driverPlayer2, 0);
+		 clickOnCell(driverPlayer1, 5);
+		 clickOnCell(driverPlayer2, 3);
+		 clickOnCell(driverPlayer1, 7);
+		 clickOnCell(driverPlayer2, 6);
+		 
+		 Thread.sleep(1000);  // Gives time to show the alert
+		 
+		 verifyMessages("Player2 wins! Player1 looses.");
+	 }
+
+	 @Test
+	 public void DrawTest1 () throws InterruptedException {
+
+		 startGame();
+		 clickOnCell(driverPlayer1, 0);
+		 clickOnCell(driverPlayer2, 1);
+		 clickOnCell(driverPlayer1, 3);
+		 clickOnCell(driverPlayer2, 4);
+		 clickOnCell(driverPlayer1, 2);
+		 clickOnCell(driverPlayer2, 8);
+		 clickOnCell(driverPlayer1, 7);
+		 clickOnCell(driverPlayer2, 6);
+		 clickOnCell(driverPlayer1, 5);
 		 
 		 Thread.sleep(1000);  // Gives time to show the alert
 
-		 String messagePlayer1 = driverPlayer1.switchTo().alert().getText();
-		 String messagePlayer2 = driverPlayer2.switchTo().alert().getText();
-		 
-		 assertThat(messagePlayer1).isEqualTo("Draw!");
-		 assertThat(messagePlayer2).isEqualTo("Draw!");
+		 verifyMessages("Draw!");
 	 }
+
+	 @Test
+	 public void DrawTest2 () throws InterruptedException {
+
+		 startGame();
+		 clickOnCell(driverPlayer1, 0);
+		 clickOnCell(driverPlayer2, 1);
+		 clickOnCell(driverPlayer1, 2);
+		 clickOnCell(driverPlayer2, 3);
+		 clickOnCell(driverPlayer1, 5);
+		 clickOnCell(driverPlayer2, 4);
+		 clickOnCell(driverPlayer1, 6);
+		 clickOnCell(driverPlayer2, 8);
+		 clickOnCell(driverPlayer1, 7);
+		 
+		 Thread.sleep(1000);  // Gives time to show the alert
+
+		 verifyMessages("Draw!");
+	 }
+
+	 @Test
+	 public void DrawTest3 () throws InterruptedException {
+
+		 startGame();
+		 clickOnCell(driverPlayer1, 0);
+		 clickOnCell(driverPlayer2, 1);
+		 clickOnCell(driverPlayer1, 2);
+		 clickOnCell(driverPlayer2, 4);
+		 clickOnCell(driverPlayer1, 3);
+		 clickOnCell(driverPlayer2, 6);
+		 clickOnCell(driverPlayer1, 5);
+		 clickOnCell(driverPlayer2, 8);
+		 clickOnCell(driverPlayer1, 7);
+		 
+		 Thread.sleep(1000);  // Gives time to show the alert
+
+		 verifyMessages("Draw!");
+	 }
+
 }
