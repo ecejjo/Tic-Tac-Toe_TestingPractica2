f86ca53e0dad1be7044a1e44ddacf05fde1296fc
Francisco Javier Jiménez Olivares <francisco.javier.jimenez@ericsson.com>
2019-04-26 00:00:03 +0200

    Prueba con dobles (TicTacToeGameTest) implementadas

--- src/test/java/es/codeurjc/ais/tictactoe/TicTacToeGameTest.java
+++ src/test/java/es/codeurjc/ais/tictactoe/TicTacToeGameTest.java
@@ -1,5 +1,6 @@
 package es.codeurjc.ais.tictactoe;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.mockito.Mockito.mock;
@@ -12,14 +13,28 @@ import static org.mockito.hamcrest.MockitoHamcrest.argThat;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 
-import es.codeurjc.ais.tictactoe.TicTacToeGame.CellMarkedValue;
 import es.codeurjc.ais.tictactoe.TicTacToeGame.EventType;
 import es.codeurjc.ais.tictactoe.TicTacToeGame.WinnerValue;
 
 public class TicTacToeGameTest {
+	
+	@Test
+	public void player1WinsTest() throws InterruptedException {
+		ticTacToeRunTest(new int[] { 0, 3, 1, 7, 2 }, 0);
+	}
+	
+	@Test
+	public void player2WinsTest() throws InterruptedException {
+		ticTacToeRunTest(new int[] { 0, 3, 7, 4, 2, 5 }, 1);
+	}
 
 	@Test
-	public void testTicTacToeGame() {
+	public void drawTest() throws InterruptedException {
+		ticTacToeRunTest(new int[] { 0, 1, 2, 4, 3, 6, 5, 8, 7 }, -1);
+	}
+
+
+	public void ticTacToeRunTest(int[] cells, int winner) {
 		
 		// 1) Crear el objeto TicTacToeGame
 		TicTacToeGame ticTacToeGame = new TicTacToeGame();
@@ -36,49 +51,62 @@ public class TicTacToeGameTest {
 		Player player1 = new Player(0, "X", "player1");
 		Player player2 = new Player(1, "O", "player2");
 		
-		// 5) Añadir los jugadores al objeto TicTacToeGame
+		// 5.1) Añadir los jugadores (player1) al objeto TicTacToeGame
 		ticTacToeGame.addPlayer(player1);
+		
+		// 6.1) Comprobar que la conexión 1 recibe el evento JOIN_GAME, con player1
+		verify(connection1).sendEvent(eq(EventType.JOIN_GAME), argThat(hasItems(player1)));
+		
+		// 7.1) Comprobar que la conexión 2 recibe el evento JOIN_GAME, con player1
+		verify(connection2).sendEvent(eq(EventType.JOIN_GAME), argThat(hasItems(player1)));
+
+		// 5.2) Añadir los jugadores (player2) al objeto TicTacToeGame
 		ticTacToeGame.addPlayer(player2);
-		
-		// 6) Comprobar que la conexión 1 recibe el evento JOIN_GAME, con ambos jugadores
+
+		// 6.2) Comprobar que la conexión 1 recibe el evento JOIN_GAME, con ambos jugadores
 		verify(connection1, times(2)).sendEvent(eq(EventType.JOIN_GAME), argThat(hasItems(player1, player2)));
-		reset(connection1);
 		
-		// 7) Comprobar que la conexión 2 recibe el evento JOIN_GAME, con ambos jugadores
+		// 7.2) Comprobar que la conexión 2 recibe el evento JOIN_GAME, con ambos jugadores
 		verify(connection2, times(2)).sendEvent(eq(EventType.JOIN_GAME), argThat(hasItems(player1, player2)));
-		reset(connection2);
 				
 		// 8) Por turnos, cada jugador va marcando una casilla invocando el método mark de
 	    //    TicTacToeGame, comprobando que el turno cambia
-		ticTacToeGame.mark(0);
-		ArgumentCaptor<CellMarkedValue> argumentCellMarkedValue = ArgumentCaptor.forClass(CellMarkedValue.class);
-		verify(connection1).sendEvent(eq(EventType.MARK), argumentCellMarkedValue.capture());
-		Object event = argumentCellMarkedValue.getValue();
+		
+		for (int i = 0; i < cells.length; i++) {
 
-		verify(connection1).sendEvent(eq(EventType.SET_TURN), argThat(equalTo(player2)));
-		reset(connection1);
+			if (i %2 == 0 )
+			{
+				verify(connection1).sendEvent(eq(EventType.SET_TURN), argThat(equalTo(player1)));
+				verify(connection2).sendEvent(eq(EventType.SET_TURN), argThat(equalTo(player1)));
+			}
+			else
+			{
+				verify(connection1).sendEvent(eq(EventType.SET_TURN), argThat(equalTo(player2)));
+				verify(connection2).sendEvent(eq(EventType.SET_TURN), argThat(equalTo(player2)));			
+			}
+			
+			reset(connection1);
+			reset(connection2);
+			
+			ticTacToeGame.mark(cells[i]);
 
-		ticTacToeGame.mark(1);
-		verify(connection2).sendEvent(eq(EventType.SET_TURN), argThat(equalTo(player2)));
-		reset(connection2);
-
-		ticTacToeGame.mark(3);
-		verify(connection1).sendEvent(eq(EventType.SET_TURN), argThat(equalTo(player1)));
-		reset(connection1);
-
-		ticTacToeGame.mark(4);
-		verify(connection2).sendEvent(eq(EventType.SET_TURN), argThat(equalTo(player2)));
-		reset(connection2);
-
-		ticTacToeGame.mark(6);
-		verify(connection1).sendEvent(eq(EventType.SET_TURN), argThat(equalTo(player1)));
-		reset(connection1);
+		}
 
 		// 9) Al final se comprueba que el juego acaba y que dependiendo de las casillas marcadas
 		//	  uno de los jugadores gana o hay empate.
 		ArgumentCaptor<WinnerValue> argumentWinnerValue = ArgumentCaptor.forClass(WinnerValue.class);
-		verify(connection2).sendEvent(eq(EventType.GAME_OVER), argumentWinnerValue.capture());
-		event = argumentWinnerValue.getValue();
+		verify(connection1).sendEvent(eq(EventType.GAME_OVER), argumentWinnerValue.capture());
+		
+		// Comprueba si se espera y realmente ha ocurrido un empate.
+		if(winner == -1) {
+			assertThat(argumentWinnerValue.getValue()).isNull();	
+		}
+		else
+		{
+			// Comprueba el ganador esperado
+			assertThat(argumentWinnerValue.getValue().player.getId()).isEqualTo(winner);
+		}
+
 	}
 
 }
